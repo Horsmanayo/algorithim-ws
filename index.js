53e15151df0a7a00dbf3a5e2e1c0e5970b8a9fa3
Horsmanayo <oniademola001@gmail.com>
2021-12-08 10:45:44 +0100

    algorithim-ws

---
+++ index.js
@@ -0,0 +1,29 @@
+let sentence = 'my very heart may just ;ovr gertility'
+
+
+
+
+
+function countSentence(text){
+    let result = {
+      sentenceLength: 0,
+      noOfWords: 0,
+      vowelCount: 0,
+    }
+    
+    result.sentenceLength = text.length
+    
+    for(let i = 0; i < text.length; i++){
+      if (text[i] == ' ' || text[i] == '.'){
+        result.noOfWords += 1
+      }
+    }
+    
+    
+    let newSentence = Array.from(text.toLowerCase())
+    result.vowelCount = newSentence.filter(letter => 'aeiou'.includes(letter)).length
+    return result
+    
+    }
+    
+    console.log(countSentence(sentence))
